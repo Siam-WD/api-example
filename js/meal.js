cf80f41f800da275d15102af8728b70561889bc0
Siam-WD <abusayedalsiam2023@gmail.com>
2023-02-24 17:09:35 +0600

    async await  complete

--- js/meal.js
+++ js/meal.js
@@ -6,7 +6,7 @@ const loadMeals = (searchText) =>{
 }
 
 const displayMeals = meals =>{
-    // console.log(meals);
+    console.log(meals[0]);
     // step 1 : need container 
     const mealsContainer = document.getElementById('container');
     
@@ -24,10 +24,14 @@ const displayMeals = meals =>{
             <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetailsModal">
+                Details
+                </button>
             </div>
+            
         </div>
         `;
-       
+
         mealsContainer.appendChild(mealDiv);
 
     });
@@ -36,9 +40,41 @@ const displayMeals = meals =>{
 
 const searchMeals = () =>{
     const searchText = document.getElementById('search-field').value;
-    console.log(searchText)
     loadMeals(searchText);
+}
+
+const loadMealDetail = idMeal => {
+    const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetaiil(data.meals[0]))
+        .catch(error => console.log(error))
+};
+// 27 number line a call hobe loadmealdetails2  
+// async await      load korar onno system
+const loadMealDetail2 = async(idMeal) => {
+    const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        displayMealDetaiil(data.meal[0]);
+    }
+    catch (error){
+        console.log(error);
+    }
+}
+
+const displayMealDetaiil = meal => {
+    document.getElementById('mealDetailsModalLabel').innerText = meal.strMeal ;
+    
+    const mealDetails =  document.getElementById('meal-detail-body');
+    mealDetails.innerHTML = `
+        <img class="img-fluid" src="${meal.strMealThumb}">
+        <h5>Location: ${meal.strArea}</h5>
+        <p>${meal.strInstructions}</p>
+    `;
 }
 
 // ny default all item show koranor jonne empty string diye call kora hoise
 loadMeals('');
+
